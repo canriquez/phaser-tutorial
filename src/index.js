0aa191706412d8fcd89a61e55f4da26eab63d960
Carlos Anriquez <canriquez@gmail.com>
2020-08-08 11:15:56 -0300

    moving and jumpling player

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            //gravity: { y: 300 },
             debug: false
         }
     },
@@ -19,6 +19,10 @@ const config = {
         update: update
     }
 };
+
+var platforms;
+var player;
+var cursors;
 
 const game = new Phaser.Game(config);
 
@@ -33,7 +37,7 @@ function preload() {
     );
 }
 
-var platforms;
+
 
 function create() {
     this.add.image(400, 300, 'sky');
@@ -45,7 +49,61 @@ function create() {
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.8);
+    player.setCollideWorldBounds(true);
+    player.body.setGravityY(300)
+
+
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    this.physics.add.collider(player, platforms);
+
 }
 
 function update() {
+
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-630);
+    }
+
 }
